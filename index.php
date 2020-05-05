55d915c38b4514f537fd0d9ee14d7135e6aaf803
Rathi <rathiram7583@gmail.com>
2020-05-05 12:26:19 -0600

    Added

--- index.php
+++ index.php
@@ -1,19 +1,34 @@
 <?php
     session_start();
 
+    if (isset($_POST['reset'])) {
+        session_unset();
+        session_destroy();
+        session_start();
+    }
+    
+
     if( !isset($_SESSION['task']))
     {
 
          $_SESSION['task']=array();
+         $_SESSION['taskcompleted']=array();
   }
 $_SESSION['task']=array_values($_SESSION['task']);
+$_SESSION['taskcompleted']=array_values($_SESSION['taskcompleted']);
+
 
 if ( isset( $_POST ) && !empty( $_POST ) )
 {
-    $submittedUsername = $_POST['task'];
+
     array_push( $_SESSION['task'], $_POST['task'] );
 }
- 
+foreach ( $_SESSION['task'] as $completedTask ) {
+    if ( isset( $_POST[$completedTask] ) ) {
+      array_push($_SESSION['taskcompleted'], $completedTask);
+      
+    }
+  }
    
 ?>
 <!DOCTYPE html>
@@ -30,20 +45,32 @@ if ( isset( $_POST ) && !empty( $_POST ) )
     <form action="index.php" method="POST" >
 
     <input type="text" name="task" placeholder="Enter the Task" class="task_input"  id="task"/>
-    <button type="submit" name="submit" class="add_task" >ADD TASK</button>
+    <button type="submit" name="submit" class="add_task"  >ADD TASK</button>
+    <button type="submit" name="reset"  class="reset_task">RESET</button>
     </form>
     <?php if ( !empty( $_SESSION['task'] ) ) :?>
     <h2>My Task :</h2>
     <ul>
     <?php foreach($_SESSION['task'] as $task) :?>
     <li>
-    <input type="checkbox" name='taskcompleted'/>
+    <input type="checkbox"  name="taskcompleted" id="taskcompleted"/>
     <?php echo $task?>
-    <button <? php unset($task) ?>>Delete</button>
+    <button>Delete</button>
     </li>
     <?php endforeach;?>
     </ul>
     <?php endif;?>
+    <?php if(isset($_POST['taskcompleted'])&& !empty($_SESSION['taskcompleted'])) :?>
+     <h2>Completed Task: </h2>
+     <ul>
+     <?php foreach($_SESSION['taskcompleted'] as $taskcompleted) :?>
+     <li>
+     <? php echo  $taskcompleted?>
+     </li>
+     <?php endforeach;?>
+     </ul>
+    <?php endif;?>
+
 
     
 
